5c0a97021ff8c3c5a2ede15a68c0e707609e8831
arisannjayaa <wayanarisanjaya01@gmail.com>
2023-06-01 07:44:44 +0800

    view berita details

--- resources/views/beranda/berita-details.blade.php
+++ resources/views/beranda/berita-details.blade.php
@@ -9,39 +9,43 @@
 @endpush
 @section('content')
     @isset($berita)
-        <div class="row flex-column mt-lg-5 mt-2">
-            <div class="col-lg-12 col-12 text-center">
-                <h1 class="display-6 fw-bold mb-3">{{ $berita->judul }}</h1>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-lg-5 col-12">
-                <div class="ratio ratio-4x3">
-                    <img style="object-fit: cover;" class="img-fluid rounded-4 shadow-sm"
-                        src="{{ asset('storage/berita/') . '/' . $berita->gambar }}" alt="">
+        <div class="container">
+            <div class="row flex-column mt-lg-5 mt-2">
+                <div class="col-lg-12 col-12 text-lg-center text-left">
+                    <h1 class="fw-bold mb-3 text-truncate fs-1 text-wrap">{{ $berita->judul }}</h1>
                 </div>
-                <div class="d-flex gap-2 mt-3 justify-content-between">
-                    <p class="text-sm"><i
-                            class="bi bi-calendar3 me-2"></i>{{ \Carbon\Carbon::parse($berita->created_at)->format(\Carbon\Carbon::now()->year == \Carbon\Carbon::parse($berita->created_at)->year ? 'd M' : 'd M Y') }}
-                    </p>
-                    <p class="text-sm"><i class="bi bi-pencil-square me-2"></i>Ditulis oleh Administrator</p>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-5 col-12">
+                    <div class="ratio ratio-4x3">
+                        <img style="object-fit: cover;" class="img-fluid rounded-4 shadow-sm"
+                            src="{{ asset('storage/berita/') . '/' . $berita->gambar }}" alt="">
+                    </div>
+                    <div class="d-flex gap-2 mt-3 justify-content-between">
+                        <p class="text-sm"><i
+                                class="bi bi-calendar3 me-2"></i>{{ \Carbon\Carbon::parse($berita->created_at)->format(\Carbon\Carbon::now()->year == \Carbon\Carbon::parse($berita->created_at)->year ? 'd M' : 'd M Y') }}
+                        </p>
+                        <p class="text-sm"><i class="bi bi-pencil-square me-2"></i>Ditulis oleh Administrator</p>
+                    </div>
                 </div>
             </div>
         </div>
     @endisset
     <div class="">
         <hr>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('beranda.berita') }}">Berita</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{ $berita->judul }}</li>
-        </ol>
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('beranda.berita') }}">Berita</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{ $berita->judul }}</li>
+            </ol>
+        </div>
         <hr class="mb-0">
     </div>
-    <div class="container-sm mt-3">
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-6 col-12 border-lg-start border-lg-end">
-                <div class="p-lg-4 p-0">
-                    <p class="text-wrap">{{ strip_tags($berita->deskripsi) }}</p>
+            <div class="col-lg-6 col-12 border-start border-end bg-white">
+                <div class="p-lg-4 px-0 py-3">
+                    <div class="text-wrap">{!! $berita->deskripsi !!}</div>
                 </div>
             </div>
         </div>
