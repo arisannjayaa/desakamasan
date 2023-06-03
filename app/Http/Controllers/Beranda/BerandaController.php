6ea670976ef430989880a4722052773ec0397615
arisannjayaa <wayanarisanjaya01@gmail.com>
2023-06-03 21:20:06 +0800

    daerah-wisata

--- app/Http/Controllers/Beranda/BerandaController.php
+++ app/Http/Controllers/Beranda/BerandaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Beranda;
 
 use App\Models\Berita;
+use App\Models\Daerah;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
@@ -27,8 +28,42 @@ class BerandaController extends Controller
 
     public function details_berita(String $slug)
     {
-        $berita = Berita::where('slug', $slug)->first();
-        // dd($berita);
-        return view('beranda.berita-details', compact('berita'));
+        $data = [
+            'berita' => Berita::where('slug', $slug)->first(),
+            'berita_all' => Berita::latest()->take(5)->get()
+        ];
+
+        return view('beranda.berita-details', $data);
+    }
+
+    public function daerah()
+    {
+        $daerahLast = Daerah::latest()->first();
+        $daerahLast->gambar = json_decode($daerahLast->gambar);
+        $daerahAll = Daerah::orderBy('created_at', 'DESC')
+                    ->paginate(12);
+
+        $daerahAll->getCollection()->transform(function ($item) {
+            $item->gambar = json_decode($item->gambar);
+            return $item;
+        });
+
+        $data = [
+            'menu' => 'Daerah',
+            'daerah_simple' => Daerah::orderBy('created_at', 'desc')->simplePaginate(12),
+            'daerah_all' => $daerahAll,
+            'daerah_last' => $daerahLast
+        ];
+        return view('beranda.daerah', $data);
+    }
+
+    public function details_daerah(String $slug)
+    {
+        $data = [
+            'daerah' => Daerah::where('slug', $slug)->first(),
+            'daerah_all' => Daerah::latest()->take(5)->get()
+        ];
+
+        return view('beranda.daerah-details', $data);
     }
 }
