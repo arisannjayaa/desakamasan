8383e01ca9d05fe803d148eb37d6f764a64b3d27
arisannjayaa <wayanarisanjaya01@gmail.com>
2023-06-08 22:35:02 +0800

    halaman produk

--- app/Http/Controllers/Beranda/ProdukController.php
+++ app/Http/Controllers/Beranda/ProdukController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Beranda;
 
-use App\Http\Controllers\Controller;
+use App\Models\Produk;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProdukController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'menu' => 'Produk',
+            'produk_simple' => Produk::orderBy('created_at', 'desc')->simplePaginate(12),
+            'produk_all' => Produk::with('kategori')->orderBy('created_at', 'desc')->paginate(12),
+            'produk_last' => Produk::with('kategori')->latest()->first(),
+        ];
+        return view('beranda.produk.index', $data);
+    }
+
+    public function show(String $slug)
+    {
+        $data = [
+            'produk' => Produk::with('kategori')->where('slug', $slug)->first(),
+            'produk_all' => Produk::latest()->take(5)->get()
+        ];
+
+        return view('beranda.produk.details', $data);
+    }
 }
