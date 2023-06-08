27f6d022befb2146b57ab72ef2447f21d1dba4ec
arisannjayaa <wayanarisanjaya01@gmail.com>
2023-06-08 22:34:11 +0800

    tambah fitur pencarian by kategori

--- app/Http/Controllers/Beranda/BeritaController.php
+++ app/Http/Controllers/Beranda/BeritaController.php
@@ -29,4 +29,25 @@ class BeritaController extends Controller
 
         return view('beranda.berita.details', $data);
     }
+
+    public function tags(String $slug)
+    {
+        // Temukan kategori berdasarkan slug
+        $kategori = KategoriBerita::where('slug', $slug)->first();
+
+        // Temukan berita yang terkait dengan kategori
+        $berita = Berita::with('kategori', 'user')->whereHas('kategori', function ($query) use ($kategori) {
+            $query->where('slug', $kategori->slug);
+        })->get();
+
+        $data = [
+            'menu' => 'Berita',
+            'berita_simple' => Berita::orderBy('created_at', 'desc')->simplePaginate(12),
+            'berita_all' => Berita::with('kategori', 'user')->orderBy('created_at', 'desc')->paginate(12),
+            'berita_last' => Berita::with('kategori', 'user')->latest()->first(),
+        ];
+
+        // Kembalikan data berita dan kategori ke view
+        return view('beranda.berita.tag', compact('berita', 'kategori'));
+    }
 }
