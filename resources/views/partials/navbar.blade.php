258bcfb7079bfe912e1a263c9933b3e7cba09bbb
arisannjayaa <wayanarisanjaya01@gmail.com>
2023-06-29 21:23:40 +0800

    menambahkan nama login dan role

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -15,8 +15,8 @@
                 <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                     <div class="user-menu d-flex">
                         <div class="user-name text-end me-3">
-                            <h6 class="mb-0 text-gray-600">John Ducky</h6>
-                            <p class="mb-0 text-sm text-gray-600">Administrator</p>
+                            <h6 class="mb-0 text-gray-600">{{ Str::ucfirst(Auth::user()->username) }}</h6>
+                            <p class="mb-0 text-sm text-gray-600">{{ Str::ucfirst(Auth::user()->role) }}</p>
                         </div>
                         <div class="user-img d-flex align-items-center">
                             <div class="avatar avatar-md">
@@ -36,7 +36,8 @@
                             Settings</a></li>
                     <hr class="dropdown-divider">
                     </li>
-                    <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-box-arrow-left me-2"></i>
+                    <li><a class="dropdown-item" href="{{ route('auth.logout') }}"><i
+                                class="icon-mid bi bi-box-arrow-left me-2"></i>
                             Logout</a></li>
                 </ul>
             </div>
