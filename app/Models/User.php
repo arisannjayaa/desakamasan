38085327c78342c0f49c14059ed346e52df4e995
arisannjayaa <wayanarisanjaya01@gmail.com>
2023-06-29 21:22:11 +0800

    melakukan extends authenticatable

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
     protected $table = 'user';
